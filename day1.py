8a3f72182d99d5de48a5dfdf69a7e9a4680e8136
Shangeeth7 <shanshangeeth@gmail.com>
2024-01-31 16:12:01 +0530

    test_prefect

---
+++ day1.py
@@ -0,0 +1,21 @@
+from prefect import flow,task
+
+
+@task
+def message_ret():
+    msg = "Hello Shangeeth"
+    return msg
+
+@flow
+def something():
+    msg = 10
+    return msg
+
+
+@flow
+def first_function():
+    msg = message_ret()
+    msg2 = something()
+    print(msg,msg2)
+
+first_function()
